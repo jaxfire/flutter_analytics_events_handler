6efa72ef9a216754210226ab89688c6a7651229c
jamesashdepop <81640222+jamesashdepop@users.noreply.github.com>
2022-02-15 19:05:33 +0000

    Provider version complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
+
+class TheText extends ChangeNotifier {
+  String theText = 'Initial Text';
+
+  void updateText(String newText) {
+    theText = newText;
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -8,78 +18,67 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String theText = 'Initial Text';
-
-  void theCallback(String newValue) {
-    setState(() {
-      theText = newValue;
-    });
-  }
+  TheText theText;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(theText),
+    return ChangeNotifierProvider<TheText>(
+      create: (context) => TheText(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: MyTextTitle(),
+          ),
+          body: Level1(),
         ),
-        body: Level1(theCallback, theText),
       ),
     );
   }
 }
 
+class MyTextTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<TheText>(context).theText);
+  }
+}
+
 class Level1 extends StatelessWidget {
-  final Function theCallback;
-  final String theText;
-
-  Level1(this.theCallback, this.theText);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(theCallback, theText),
+      child: Level2(),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
-  final Function theCallback;
-  final String theText;
-
-  Level2(this.theCallback, this.theText);
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        MyTextField(theCallback),
-        Level3(theText),
+        MyTextField(),
+        Level3(),
       ],
     );
   }
 }
 
 class Level3 extends StatelessWidget {
-  final String theText;
-  Level3(this.theText);
-
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(theText),
+      child: Text(Provider.of<TheText>(context).theText),
     );
   }
 }
 
 class MyTextField extends StatelessWidget {
-  final Function theCallback;
-  MyTextField(this.theCallback);
-
   @override
   Widget build(BuildContext context) {
     return TextField(
-      onChanged: theCallback,
+      onChanged: (String newValue) =>
+          Provider.of<TheText>(context, listen: false).updateText(newValue),
     );
   }
 }
