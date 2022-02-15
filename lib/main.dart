94e36b6b311f959b819413e9b38aea56deb6b305
jamesashdepop <81640222+jamesashdepop@users.noreply.github.com>
2022-02-15 16:12:03 +0000

    Ready for Callbacks and Constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Data>(
-      builder: (context) => Data(),
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: MyText(),
-          ),
-          body: Level1(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('TODO'),
         ),
+        body: Level1(),
       ),
     );
   }
@@ -44,14 +40,9 @@ class Level2 extends StatelessWidget {
 class Level3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(Provider.of<Data>(context).data);
-  }
-}
-
-class MyText extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(Provider.of<Data>(context, listen: false).data);
+    return Container(
+      child: Text('TODO'),
+    );
   }
 }
 
@@ -60,17 +51,8 @@ class MyTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextField(
       onChanged: (newText) {
-        Provider.of<Data>(context).changeString(newText);
+        // TODO: Invoke callback
       },
     );
-  }
-}
-
-class Data extends ChangeNotifier {
-  String data = 'Some data';
-
-  void changeString(String newString) {
-    data = newString;
-    notifyListeners();
   }
 }
