6ec2f396763b5e43399aa7d81dc349b2d9282538
jamesashdepop <81640222+jamesashdepop@users.noreply.github.com>
2022-02-15 16:45:26 +0000

    Callbacks and Constructors complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,57 +2,84 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String theText = 'Initial Text';
+
+  void theCallback(String newValue) {
+    setState(() {
+      theText = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('TODO'),
+          title: Text(theText),
         ),
-        body: Level1(),
+        body: Level1(theCallback, theText),
       ),
     );
   }
 }
 
 class Level1 extends StatelessWidget {
+  final Function theCallback;
+  final String theText;
+
+  Level1(this.theCallback, this.theText);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Level2(),
+      child: Level2(theCallback, theText),
     );
   }
 }
 
 class Level2 extends StatelessWidget {
+  final Function theCallback;
+  final String theText;
+
+  Level2(this.theCallback, this.theText);
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        MyTextField(),
-        Level3(),
+        MyTextField(theCallback),
+        Level3(theText),
       ],
     );
   }
 }
 
 class Level3 extends StatelessWidget {
+  final String theText;
+  Level3(this.theText);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('TODO'),
+      child: Text(theText),
     );
   }
 }
 
 class MyTextField extends StatelessWidget {
+  final Function theCallback;
+  MyTextField(this.theCallback);
+
   @override
   Widget build(BuildContext context) {
     return TextField(
-      onChanged: (newText) {
-        // TODO: Invoke callback
-      },
+      onChanged: theCallback,
     );
   }
 }
